803671e65e0ab344ceb65049c0de0aad0f689430
juzu400 <88151612+juzu400@users.noreply.github.com>
2021-08-02 01:00:47 +0300

    Update card.py

--- card.py
+++ card.py
@@ -1,20 +1,42 @@
-import random
+import sqlite3, random
+conn = sqlite3.connect('card.s3db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS card (
+    id INTEGER,
+    number TEXT,
+    pin TEXT,
+    balance INTEGER DEFAULT 0);""")
+conn.commit()
+
 
 
 class Card:
-    
     list_of_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     IIN = str(400000)
     user_choice = None
     all_cards = []
-    
+
     def __init__(self):
         account_number = "".join(map(str, random.sample(Card.list_of_digits, 9)))
-        checksum = str(random.randrange(10))
+        self.number = Card.IIN + account_number
+        algoritm = list(map(int, self.number))
+        sum_ = 0
+        for i in range(15):
+            if (i + 1) % 2 == 1:
+                algoritm[i] *= 2
+        for i in range(15):
+            if algoritm[i] > 9:
+                algoritm[i] -= 9
+        for i in range(15):
+            sum_ += algoritm[i]
+        checksum = str((10 - (sum_ % 10)) % 10)
+        # checksum = str(random.randrange(10))
         self.number = Card.IIN + account_number + checksum
         self.PIN = "".join(map(str, random.sample(Card.list_of_digits, 4)))
         self.balance = 0
-    
+        cur.execute('INSERT INTO card (number, pin, balance) VALUES (?, ?, ?)', (self.number, self.PIN, self.balance))
+        conn.commit()
+
     def main_menu():
         while True:
             print("""
@@ -31,7 +53,7 @@ class Card:
             if Card.user_choice == 0:
                 print("Bye!")
                 break
-                
+
     def create_card():
         card_instance = Card()
         Card.all_cards.append(card_instance)
@@ -39,7 +61,7 @@ class Card:
         print(card_instance.number)
         print("Your card PIN:")
         print(card_instance.PIN)
-    
+
     def log_in():
         card_number = input("Enter your card number:\n")
         PIN = input("Enter your PIN:\n")
@@ -66,6 +88,6 @@ class Card:
                     break
         else:
             print("Wrong card number or PIN!")
-            
-            
+
+
 Card.main_menu()
